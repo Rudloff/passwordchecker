9f2fb9a4c69739cffc7e875d51a11b8b89ef3f8b
Pierre Rudloff <pierre@rudloff.pro>
2013-08-18 17:25:45 +0200

    HTML valide

--- js/bigtext.js
+++ js/bigtext.js
@@ -11,7 +11,7 @@
       STYLE_ID: 'bigtext-id',
       LINE_CLASS_PREFIX: 'bigtext-line',
       EXEMPT_CLASS: 'bigtext-exempt',
-      DEFAULT_CHILD_SELECTOR: '> div',
+      DEFAULT_CHILD_SELECTOR: '> span',
       childSelectors: {
         div: '> div',
         ol: '> li',
